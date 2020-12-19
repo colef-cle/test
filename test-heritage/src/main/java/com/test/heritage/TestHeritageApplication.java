15065b5d51135e3502604a8921b0d49e8cd0aa76
Colin Lefebvre <cle@colef.fr>
2020-12-19 19:06:21 +0100

    added spy after position instanciation

--- test-heritage/src/main/java/com/test/heritage/TestHeritageApplication.java
+++ test-heritage/src/main/java/com/test/heritage/TestHeritageApplication.java
@@ -19,6 +19,8 @@ public class TestHeritageApplication {
 	public Integer getSpotStateController( SpotStateController controller ) {
 
 		Position position = new IndoorPosition();
+		
+		System.out.println("tryout something");
 		position.setAlias( "new SDF In" );
 
 		OutdoorRepairSpotState ors = new OutdoorRepairSpotState(
